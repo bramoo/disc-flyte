ae303ab01b0b75c3a1c19dbbc92d36b65ad0c291
Bradley Moorfield <brad.moorfield@gmail.com>
2021-09-17 22:34:03 +1200

    Scene update

--- src/components/ViewBox/ViewBox.jsx
+++ src/components/ViewBox/ViewBox.jsx
@@ -5,22 +5,33 @@ import { vec3 } from "../../simulation/util";
 import "./ViewBox.css";
 
 export function ViewBox(props) {
-    let vertices = [0, 0, 0];
-    let count = 0;
-    if (props.result) {
-        let points = props.result.pos_g.filter((v) => !!v).map((v) => vec3(v.x, v.y, v.z));
-        vertices = points.reduce((a, p) => a.concat([p.y, p.z, p.x]), []);
-        count = points.length;
-    }
+	let vertices = [0, 0, 0];
+	let count = 0;
+	if (props.result) {
+		let points = props.result.pos_g
+			.filter((v) => !!v)
+			.map((v) => vec3(v.x, v.y, v.z));
+		vertices = points.reduce((a, p) => a.concat([p.y, p.z, p.x]), []);
+		count = points.length;
+	}
 
-    return (
-        <div className="view-box">
-            <p>{props.result && props.result.pos_g && props.result.pos_g[2].x}</p>
-            <Canvas camera={{ position: [0, 0, -5] }}>
-                <OrbitControls enablePan={false} />
-                <axesHelper />
-                <Line color="red" points={vertices} />
-            </Canvas>
-        </div>
-    );
+	const width = 10;
+	const length = 70;
+
+	return (
+		<Canvas camera={{ position: [0, 0, -5] }}>
+			<axesHelper />
+			<OrbitControls />
+
+			<ambientLight color={0x404040} />
+			<pointLight position={[0, 20, 25]} decay={2} />
+
+			<Line color="red" points={vertices} />
+
+			<mesh position={[0, 0, length / 2]} rotation={[-Math.PI / 2, 0, 0]}>
+				<planeGeometry args={[width, length]} />
+				<meshStandardMaterial color="#00aa00" />
+			</mesh>
+		</Canvas>
+	);
 }
